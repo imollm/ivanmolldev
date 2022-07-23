4b7b77487d8d5dc6b97c6ea0f5e783d8a6c8580c
Ivan Moll <ivanmoll07@gmail.com>
2022-07-24 00:32:02 +0200

    Work in progress

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				main: '#FFC300',
+				secondary: '#FF5733',
+				third: '#C72738',
+				fourth: '#901A3D',
+				five: '#571845'
+			},
+			fontFamily: {
+				'roboto': ['Roboto', 'sans-serif'],
+				'work-sans': ['WorkSans', 'sans-serif']
+			}
+		},
 	},
 	plugins: [],
 }
